a589fd0863f3afc270dd970985b7fe0fd6ca56b9
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2024-02-20 00:02:26 +0530

    Now you can also count total number of words in text

--- Python-Lab/countTotalCharacters-Script.py
+++ Python-Lab/countTotalCharacters-Script.py
@@ -1,5 +1,7 @@
 # Initialize an empty string to store the input text
-text = ""
+text = """    """
+
+# Use of triple quotation marks to take multiline text.
 
 # Prompt the user to enter the text line by line until an empty line is entered
 print("Enter the text (press Enter twice to finish):")
@@ -15,3 +17,10 @@ total_characters = len(text)
 
 # Print the total number of characters
 print("Total number of characters (including spaces and special characters):", total_characters)
+
+# Calculate the total number of words
+words = text.split()
+total_words = len(words)
+
+# Print the total number of words
+print("Total number of words:  ", total_words)
